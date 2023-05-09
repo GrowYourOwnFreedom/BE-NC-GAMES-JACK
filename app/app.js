a1bd1fa7e882778e3d966742802cef0c34e9af95
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2023-05-09 13:59:24 +0100

    include error handling

--- app/app.js
+++ app/app.js
@@ -7,3 +7,8 @@ app.use(express.json());
 module.exports = app;
 
 app.get("/api/categories", getCategories);
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).send({ msg: "sorry, server has a problem!" });
+});
