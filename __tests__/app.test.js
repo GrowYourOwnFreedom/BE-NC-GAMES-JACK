fb2c8bafda49093b7438b3b7a2057071434a6d32
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2023-05-11 11:11:14 +0100

    refactored agin for tutor comments!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,20 +39,15 @@ describe("GET /api/reviews/:review_id", () => {
 			.get("/api/reviews/10")
 			.expect(200)
 			.then(({ body: { review } }) => {
-				const output = {
-					review_id: 10,
-					title: "Build you own tour de Yorkshire",
-					designer: "Asger Harding Granerud",
-					owner: "mallionaire",
-					review_img_url:
-						"https://images.pexels.com/photos/258045/pexels-photo-258045.jpeg?w=700&h=700",
-					review_body:
-						"Cold rain pours on the faces of your team of cyclists, you pulled to the front of the pack early and now your taking on exhaustion cards like there is not tomorrow, you think there are about 2 hands left until you cross the finish line, will you draw enough from your deck to cross before the other team shoot passed? Flamee Rouge is a Racing deck management game where you carefully manage your deck in order to cross the line before your opponents, cyclist can fall slyly behind front runners in their slipstreams to save precious energy for the prefect moment to burst into the lead ",
-					category: "social deduction",
-					created_at: "2021-01-18T10:01:41.251Z",
-					votes: 10,
-				};
-				expect(review).toEqual(output);
+				expect(typeof review.title).toBe("string");
+				expect(typeof review.designer).toBe("string");
+				expect(typeof review.owner).toBe("string");
+				expect(typeof review.review_img_url).toBe("string");
+				expect(typeof review.review_body).toBe("string");
+				expect(typeof review.category).toBe("string");
+				expect(typeof review.created_at).toBe("string");
+				expect(typeof review.votes).toBe("number");
+				expect(review.review_id).toBe(10);
 			});
 	});
 	test("GET status 404 should return {msg: not found!} if id doesent exist ", () => {
